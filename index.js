3b61d0c268e259d8b15fa5ae4564a45f44c13c58
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2025-12-19 12:53:50 +0300

    i18n

---
+++ index.js
@@ -0,0 +1,55 @@
+import { currency, price, translations } from "./i18n.js";
+
+const availableLanguages = ['de', 'en', 'es', 'fr', 'ja', 'pt']
+const fallbackLanguage = 'en'
+
+const htmlElement = document.documentElement
+
+function getFormattedPrice(locale, amount) {
+    return new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency: currency[locale]
+    }).format(amount)
+}
+
+function applyTranslations() {
+    const currentLang = htmlElement.lang
+    const curr = currency[currentLang]
+    const prices = price[curr]
+    document.querySelectorAll('[data-i18n]').forEach(element => {
+        const key = element.getAttribute('data-i18n')
+        let translation = translations[currentLang]?.[key] || translations[fallbackLanguage]?.[key] || key
+        const yearlyAccess = getFormattedPrice(currentLang, prices.yearlyAccess)
+        const yearlyPerWeek = getFormattedPrice(currentLang, prices.yearlyPerWeek)
+        const weeklyAccess = getFormattedPrice(currentLang, prices.weeklyAccess)
+
+        translation = translation
+            .replace(/{{yearlyAccess}}/g, yearlyAccess)
+            .replace(/{{yearlyPerWeek}}/g, yearlyPerWeek)
+            .replace(/{{weeklyAccess}}/g, weeklyAccess)
+        element.innerHTML = translation
+    })
+}
+
+function updateLanguage(lang) {
+    const url = new URL(window.location)
+    url.searchParams.set('lang', lang)
+    window.history.replaceState({}, '', url)
+    htmlElement.lang = lang
+    applyTranslations()
+}
+
+function initializeLanguage() {
+    const url = new URL(window.location)
+    let currentLanguage = url.searchParams.get('lang')
+
+    if (currentLanguage && availableLanguages.includes(currentLanguage)) {
+        htmlElement.lang = currentLanguage
+    } else {
+        currentLanguage = fallbackLanguage
+        updateLanguage(currentLanguage)
+    }
+    applyTranslations()
+}
+
+initializeLanguage()
