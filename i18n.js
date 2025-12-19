4145a2b5539dfaf5dd7abf82e56bcb2263e74f84
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2025-12-19 12:53:43 +0300

    init

---
+++ i18n.js
@@ -0,0 +1,125 @@
+export const translations = {
+    de: {
+        "Get Unlimited <br>Access": "Erhalten Sie unbegrenzten <br>Zugriff",
+        "Unlimited Art <br>Creation": "Unbegrenzte <br>Kunstschaffung",
+        "Exclusive <br>Styles": "Exklusive <br>Stile",
+        "Magic Avatars <br>With 20% Off": "20 % Rabatt auf <br>KI-Avatare",
+        "YEARLY ACCESS": "JÄHRLICHER ZUGRIFF",
+        "BEST OFFER": "BESTES ANGEBOT",
+        "Just {{yearlyAccess}} per year": "Nur {{yearlyAccess}} pro Jahr",
+        "{{yearlyPerWeek}}": "{{yearlyPerWeek}}",
+        "WEEKLY ACCESS": "WÖCHENTLICHER ZUGRIFF",
+        "{{weeklyAccess}} <br>per week": "{{weeklyAccess}} <br>pro Woche",
+        "Terms of Use": "Nutzungsbedingungen",
+        "Privacy Policy": "Datenschutzrichtlinie",
+        "Restore": "Wiederherstellen",
+        "Continue": "Weiter"
+    },
+    en: {
+        "Get Unlimited <br>Access": "Get Unlimited <br>Access",
+        "Unlimited Art <br>Creation": "Unlimited Art <br>Creation",
+        "Exclusive <br>Styles": "Exclusive <br>Styles",
+        "Magic Avatars <br>With 20% Off": "Magic Avatars <br>With 20% Off",
+        "YEARLY ACCESS": "YEARLY ACCESS",
+        "BEST OFFER": "BEST OFFER",
+        "Just {{yearlyAccess}} per year": "Just {{yearlyAccess}} per year",
+        "{{yearlyPerWeek}}": "{{yearlyPerWeek}}",
+        "WEEKLY ACCESS": "WEEKLY ACCESS",
+        "{{weeklyAccess}} <br>per week": "{{weeklyAccess}} <br>per week",
+        "Terms of Use": "Terms of Use",
+        "Privacy Policy": "Privacy Policy",
+        "Restore": "Restore",
+        "Continue": "Continue"
+    },
+    es: {
+        "Get Unlimited <br>Access": "Obtén acceso <br>ilimitado",
+        "Unlimited Art <br>Creation": "Ilimitada creación <br>de arte",
+        "Exclusive <br>Styles": "Estilos <br>exclusivos",
+        "Magic Avatars <br>With 20% Off": "20 % de descuento en <br>avatares de IA",
+        "YEARLY ACCESS": "ACCESO ANUAL",
+        "BEST OFFER": "MEJOR OFERTA",
+        "Just {{yearlyAccess}} per year": "Solo {{yearlyAccess}} por año",
+        "{{yearlyPerWeek}}": "{{yearlyPerWeek}}",
+        "WEEKLY ACCESS": "ACCESO SEMANAL",
+        "{{weeklyAccess}} <br>per week": "{{weeklyAccess}} <br>por semana",
+        "Terms of Use": "Términos de uso",
+        "Privacy Policy": "Política de privacidad",
+        "Restore": "Restaurar",
+        "Continue": "Continuar"
+    },
+    fr: {
+        "Get Unlimited <br>Access": "Obtenez un accès <br>illimité",
+        "Unlimited Art <br>Creation": "Création artistique <br>illimitée",
+        "Exclusive <br>Styles": "Styles <br>exclusifs",
+        "Magic Avatars <br>With 20% Off": "20&nbsp;% de réduction <br>sur les avatars IA",
+        "YEARLY ACCESS": "ACCÈS ANNUEL",
+        "BEST OFFER": "MEILLEURE OFFRE",
+        "Just {{yearlyAccess}} per year": "Seulement {{yearlyAccess}} par an",
+        "{{yearlyPerWeek}}": "{{yearlyPerWeek}}",
+        "WEEKLY ACCESS": "ACCÈS HEBDOMADAIRE",
+        "{{weeklyAccess}} <br>per week": "{{weeklyAccess}} <br>par semaine",
+        "Terms of Use": "Conditions d’utilisation",
+        "Privacy Policy": "Politique de confidentialité",
+        "Restore": "Restaurer",
+        "Continue": "Continuer"
+    },
+    ja: {
+        "Get Unlimited <br>Access": "無制限アクセス<br>を入手",
+        "Unlimited Art <br>Creation": "アート作品を<br>無制限に創造",
+        "Exclusive <br>Styles": "特別な<br>スタイル",
+        "Magic Avatars <br>With 20% Off": "AIアバターが<br>20%オフ",
+        "YEARLY ACCESS": "年間アクセス",
+        "BEST OFFER": "ベストオファー",
+        "Just {{yearlyAccess}} per year": "わずか{{yearlyAccess}}/年",
+        "{{yearlyPerWeek}}": "{{yearlyPerWeek}}",
+        "WEEKLY ACCESS": "週ごとのアクセス",
+        "{{weeklyAccess}} <br>per week": "{{weeklyAccess}}/週<br>",
+        "Terms of Use": "利用規約",
+        "Privacy Policy": "プライバシーポリシー",
+        "Restore": "復元する",
+        "Continue": "続行する"
+    },
+    pt: {
+        "Get Unlimited <br>Access": "Obtenha Acesso <br>Ilimitado",
+        "Unlimited Art <br>Creation": "Criação de Arte <br>Ilimitada",
+        "Exclusive <br>Styles": "Estilos <br>Exclusivos",
+        "Magic Avatars <br>With 20% Off": "20% de Desconto em <br>Avatares de IA",
+        "YEARLY ACCESS": "ACESSO ANUAL",
+        "BEST OFFER": "MELHOR OFERTA",
+        "Just {{yearlyAccess}} per year": "Apenas {{yearlyAccess}} por ano",
+        "{{yearlyPerWeek}}": "{{yearlyPerWeek}}",
+        "WEEKLY ACCESS": "ACESSO SEMANAL",
+        "{{weeklyAccess}} <br>per week": "{{weeklyAccess}} <br>por semana",
+        "Terms of Use": "Termos de Uso",
+        "Privacy Policy": "Política de Privacidade",
+        "Restore": "Restaurar",
+        "Continue": "Continuar"
+    },
+}
+
+export const currency = {
+    de: 'EUR',
+    en: 'USD',
+    es: 'EUR',
+    fr: 'EUR',
+    ja: 'JPY',
+    pt: 'EUR'
+}
+
+export const price = {
+    USD: {
+        yearlyAccess: 39.99,
+        yearlyPerWeek: 0.48,
+        weeklyAccess: 6.99,
+    },
+    EUR: {
+        yearlyAccess: 34.14,
+        yearlyPerWeek: 0.41,
+        weeklyAccess: 5.97,
+    },
+    JPY: {
+        yearlyAccess: 6277.11,
+        yearlyPerWeek: 75.34,
+        weeklyAccess: 1097.20,
+    }
+}
